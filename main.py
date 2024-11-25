a257e8f37fc04a58972de5026878633e6fe65154
Assol-Zakirova <zakirovaassol@gmail.com>
2024-11-25 18:22:46 +0600

    Fourth commit
    I added ship generator

--- main.py
+++ main.py
@@ -1,12 +1,130 @@
-import os
+################################################################################
+import random
 
-ships = [ [(0,1)], [(0,4)], [(3,0)], [(6,5)], [(2,5), (3,5)], [(6,0), (6,1)], [(2,3),(3,3),(4,3)]  ]
+ships = []
+freezone = set()
+
+def create_freezone():
+    global freezone
+    freezone = set()
+    for i in range(7):
+        for j in range(7):
+            freezone.add((i, j))
+
+def random_direction():
+    direction = random.randint(0, 1)
+    return direction
+
+def del_from_freezone(coordinates):
+    for coordinate in coordinates:
+        freezone.discard(coordinate)
+        x, y = coordinate[0], coordinate[1]
+        freezone.discard((x + 1, y))
+        freezone.discard((x - 1, y))
+        freezone.discard((x, y + 1))
+        freezone.discard((x, y - 1))
+        freezone.discard((x + 1, y + 1))
+        freezone.discard((x - 1, y - 1))
+        freezone.discard((x + 1, y - 1))
+        freezone.discard((x - 1, y + 1))
+
+def big_ship():
+    global freezone
+    global ships
+    if random_direction() == 1:
+        first_cell = (random.randint(0, 6), random.randint(0, 4))
+        second_cell = (first_cell[0], first_cell[1] + 1)
+        third_cell = (first_cell[0], first_cell[1] + 2)
+        ships.append([first_cell, second_cell, third_cell])
+        del_from_freezone([first_cell, second_cell, third_cell])
+    else:
+        first_cell = (random.randint(0, 4), random.randint(0, 6))
+        second_cell = (first_cell[0] + 1, first_cell[1])
+        third_cell = (first_cell[0] + 2, first_cell[1])
+        ships.append([first_cell, second_cell, third_cell])
+        del_from_freezone([first_cell, second_cell, third_cell])
+
+def medium_ship():
+    global ships
+    global freezone
+    if random_direction() == 1:
+        first_cell = random.choice(list(freezone))
+        if 0 <= first_cell[0] <= 6 and 0 <= first_cell[1] <= 5:
+            second_cell = (first_cell[0], first_cell[1] + 1)
+            if second_cell in freezone:
+                ships.append([first_cell, second_cell])
+                del_from_freezone([first_cell, second_cell])
+            else:
+                medium_ship()
+                return 0
+        else:
+            medium_ship()
+            return 0
+
+    else:
+        first_cell = random.choice(list(freezone))
+        if 0 <= first_cell[0] <= 5 and 0 <= first_cell[1] <= 6:
+            second_cell = (first_cell[0] + 1, first_cell[1])
+            if second_cell in freezone:
+                ships.append([first_cell, second_cell])
+                del_from_freezone([first_cell, second_cell])
+            else:
+                medium_ship()
+                return 0
+        else:
+            medium_ship()
+            return 0
+
+
+def one_ship():
+    global ships
+    global freezone
+    first_cell = random.choice(list(freezone))
+    ships.append([first_cell])
+    del_from_freezone([first_cell])
+
+
+def last_one_ship():
+    global ships
+    global freezone
+    if len(freezone) == 0:
+        ships = []
+        create_freezone()
+        big_ship()
+        medium_ship()
+        medium_ship()
+        one_ship()
+        one_ship()
+        one_ship()
+        last_one_ship()
+    else:
+        first_cell = random.choice(list(freezone))
+        ships.append([first_cell])
+        del_from_freezone([first_cell])
+
+def field_generation():
+    global ships
+    ships = []
+    create_freezone()
+    big_ship()
+    medium_ship()
+    medium_ship()
+    one_ship()
+    one_ship()
+    one_ship()
+    last_one_ship()
+    ship_coordinate = ships
+    return ship_coordinate
+################################################################################
+
+import os
+ships = field_generation()
 
 all_ship_coordinates = []
 for ship in ships:
     for coordinate in ship:
         all_ship_coordinates.append(coordinate)
-print(all_ship_coordinates)
+print(ships)
 
 player_coordinates = []
 sunk_counter = 0
@@ -96,6 +214,7 @@ choice = input('Do you wanna restart the game? (Yes/No): ')
 while choice == 'Yes':
     player_coordinates = []
     field = [['*' for i in range(7)] for j in range(7)]
+    ships = field_generation()
     shots = 0
     sunk_counter = 0
     sea_battle()
